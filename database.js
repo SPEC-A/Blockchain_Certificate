59d0a293dabf44e0294a912df1c61b633cc6e143
Yangruyeon <72532234+Yangruyeon@users.noreply.github.com>
2022-02-14 17:35:17 +0900

    Add files via upload

--- database.js
+++ database.js
@@ -1,12 +1,28 @@
 var mysql = require('mysql');
-var db_info = {
+var conn = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: '1111',
+    password: '',
     database: 'mydata'
-}
+});
 
+conn.connect();
+
+// db 객체를 main.js에서 사용할 수 있게 모듈로 export 하기
+module.exports = conn;
+
+// cmd 창에 user_info 테이블에 있는 정보를 보여줌
+conn.query('SELECT * FROM user_info', function(error, results, fields){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+});
+
+conn.end();
+
+/*
 module.exports = {
     init: function () {
         return mysql.createConnection(db_info);
@@ -18,3 +34,4 @@ module.exports = {
         });
     }
 }
+*/
